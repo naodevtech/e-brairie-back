551f5bfd97737461d0379fa51e859b4893dcb858
Chuck <lorris.dhedin@hotmail.fr>
2021-04-20 10:06:10 +0200

    init sequelize

---
+++ src/config/db/config/database.js
@@ -0,0 +1,9 @@
+module.exports = {
+  development: {
+    username: 'root',
+    password: 'password',
+    database: 'E-brairy',
+    host: 3306,
+    dialect: 'mysql'
+  }
+};
--- src/config/db/database.js
+++ src/config/db/database.js
@@ -5,6 +5,7 @@ const sequelize = new Sequelize(
   config.db_name,
   config.db_user,
   config.db_password,
+  config.db_host,
   { dialect: 'mysql' }
 );
 const db = { sequelize, Sequelize };
